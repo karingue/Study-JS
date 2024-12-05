0440f101553661c95ae57ca1ba254f4773bb09fa
Gabriel Karing <gabhenkar@gmail.com>
2024-12-05 14:56:56 -0300

    New Commit
    Some types of Events

--- js/index.js
+++ js/index.js
@@ -1,12 +1,31 @@
-const carro = {
-    marca:"ford", 
-    modelo:"ka", 
-    ano:2015, 
-    placa:"ABC-1234",
-    buzina: function() { alert("Biiiiiiiiiiiii")},
-    completo:function(){
-        return "A marca é " +this.marca+" e o modelo é: " +this.modelo;
-    }
-};
+function eventoClick(){
+    // alert('Acionou um evento de click');
+    document.body.style.backgroundColor = "yellow";
+}
 
-console.log(carro.completo()); 
+function eventoDblClick(){
+    document.body.style.backgroundColor = "red";
+}
+
+function viraVermelho(){
+    let div = document.getElementById("teste");
+    div.style.backgroundColor = "red";
+}
+function viraAzul(){
+    let div = document.getElementById("teste");
+    div.style.backgroundColor = "blue";
+}
+
+// function adicionarTexto(){
+//     let p = document.getElementById("texto");
+//     p.append('O mouse moveu <br>');
+// }
+
+function mudou(){
+    console.log('Mudou');
+}
+
+function telacPressionada(){
+    let input = document.getElementById("campoTexto").value;
+    console.log(input);
+}
