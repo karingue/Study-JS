005113fcfe7937579651ee4c0e46b77c887c30a4
Gabriel Karing <gabhenkar@gmail.com>
2024-12-06 13:58:50 -0300

    new commit

--- js/index.js
+++ js/index.js
@@ -1,13 +1,40 @@
-function verificar(){
-    let nome = document.getElementById("nome").value;
+function diaDaSemana(){
+    var dia = new Date().getDay();
+    
+    console.log(dia);
+    
+    switch(dia){
+        case 0:
+            document.getElementById("teste").innerHTML = "Hoje é domingo";
+            break;
 
-    if(nome == "" || nome==null){
-    let p = document.getElementById("teste");
-        p.innerHTML = "O campo não pode ser vazio";
-        p.style.color = "red";
-    }else{
-        let p = document.getElementById("teste");
-        p.innerHTML = "Parabens tudo certo!";
-        p.style.color = "green";
+        case 1:            
+            document.getElementById("teste").innerHTML = "Hoje é segunda";
+            break;
+
+        case 2:                    
+        
+            document.getElementById("teste").innerHTML = "Hoje é terça";
+            break;
+
+        case 3:
+            document.getElementById("teste").innerHTML = "Hoje é quarta";
+            break;
+        
+        case 4:
+
+            document.getElementById("teste").innerHTML = "Hoje é quinta";   
+            break;
+
+        case 5:
+
+            document.getElementById("teste").innerHTML = "Hoje é sexta";
+            break;
+    
+        case 6:
+            document.getElementById("teste").innerHTML = "Hoje é sabado";         
+            break;
+        default:
+            document.getElementById("teste").innerHTML = "Hoje é uma semana";
     }
 }
