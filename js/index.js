5d07859ad20a3a94e934ca61f447e79169b412b2
Gabriel Karing <gabhenkar@gmail.com>
2024-12-09 10:29:54 -0300

    new commit

--- js/index.js
+++ js/index.js
@@ -1,7 +1,17 @@
-var v1, v2, v3;
+class Carro{
+    constructor(valor1,valor2,valor3){
+        this.marca = valor1;
+        this.modelo = valor2;
+        this.ano = valor3;
+    }
+    buzina(){
+        return this.modelo + " Buzinou: Biiiiiiiiiiiii";
+    }
+}
 
-v1 = 10;
-v2 = "10";
-
-total = (v1 === v2); //Verifica se o tipo da variavel é igual
-console.log(total)
+const uno = new Carro("Fiat","Uno",2001); 
+const gol = new Carro("Volkswagen", "Gol", 2007);
+console.log(uno); 
+console.log(gol);
+gol.ano = 2014; 
+console.log(gol);
